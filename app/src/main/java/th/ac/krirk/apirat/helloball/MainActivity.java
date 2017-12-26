33acc9e53023a8b1bb93a60af5e0a11e78354f70
Apirat <apirat.on2018@gmail.com>
2017-12-26 12:17:06 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/krirk/apirat/helloball/MainActivity.java
@@ -0,0 +1,26 @@
+package th.ac.krirk.apirat.helloball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import th.ac.krirk.apirat.helloball.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//      add fragment to activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+
+
+        }
+
+    }   //Main Method
+}   // Main Class
